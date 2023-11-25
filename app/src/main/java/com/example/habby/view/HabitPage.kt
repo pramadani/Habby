8ae330213b8f350a7a24ebca038b2cf3a76dda69
AzaryaSantoso123 <qwertyuiop221103@gmail.com>
2023-11-25 16:52:10 +0700

    ui1

--- app/src/main/java/com/example/habby/view/HabitPage.kt
+++ app/src/main/java/com/example/habby/view/HabitPage.kt
@@ -1,16 +1,20 @@
 package com.example.habby.view
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.habby.model.Habit
 import com.example.habby.notification.Notification
 import com.example.habby.viewmodel.HabitViewModel
@@ -19,7 +23,14 @@ import com.example.habby.viewmodel.HabitViewModel
 fun HabitPage(viewModel: HabitViewModel) {
     val habits = viewModel.habitList.collectAsState().value
 
-    Column {
+    Column(
+        modifier = Modifier
+            .width(1080.dp)
+            .height(2340.dp)
+            .background(color = Color(0xFF121317)),
+        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+    ) {
         Button(
             onClick = {
                 viewModel.insertHabit(
@@ -29,30 +40,59 @@ fun HabitPage(viewModel: HabitViewModel) {
         ) {
             Text("Add Habit")
         }
-
-
         val service = Notification(LocalContext.current)
 
         Button(onClick = { service.showNotification() }) {
-            Text(text = "Show Notification")
+            Text(text = "Show Notification")}
+
+        Column(
+            modifier = Modifier
+                .width(1080.dp)
+                .height(1837.dp)
+                .padding(0.dp)
+                .background(
+                    color = Color(0xFF08184c), shape = RoundedCornerShape(
+                        topStart = 24.dp,
+                        topEnd = 24.dp,
+                        bottomEnd = 0.dp,
+                        bottomStart = 0.dp
+                    )
+                )
+            ,
+            verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
+            horizontalAlignment = Alignment.Start)
+        {
+            HabitList(
+                habits = habits
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
         }
 
-        HabitList(
-            habits = habits
-        )
     }
 
 }
-
 @Composable
 fun HabitList(
     habits: List<Habit>,
 ) {
     LazyColumn {
         items(habits) { habit ->
-            HabitItem(
-                habit = habit
+            Row(modifier = Modifier
+                .padding(10.dp)
+                .width(500.dp)
+                .height(100.dp)
+                .background(color = Color(0xFF397CFF), shape = RoundedCornerShape(size = 50.dp))
+                .padding(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.Start),
+                verticalAlignment = Alignment.Top,
             )
+            {
+                HabitItem(
+                    habit = habit
+                )
+            }
+
         }
     }
 }
